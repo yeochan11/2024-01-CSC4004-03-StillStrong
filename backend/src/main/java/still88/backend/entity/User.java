218c1df378cd640d4f8a611e7f7eed45f2cf25ba
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-04-15 14:29:43 +0900

    feat: User Entity 생성

--- backend/src/main/java/still88/backend/entity/User.java
+++ backend/src/main/java/still88/backend/entity/User.java
@@ -1,4 +1,20 @@
 package still88.backend.entity;
 
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "User")
 public class User {
+    @Id
+    private Integer userId;
+    private String userNickname;
+    private Integer userAge;
+    private Boolean userGender;
+    private String userImage;
 }
