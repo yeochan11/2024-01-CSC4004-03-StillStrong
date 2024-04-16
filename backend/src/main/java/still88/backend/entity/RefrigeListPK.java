e2f4d4de64aaa724a032468809b81157184793e8
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-04-16 15:40:07 +0900

    feat: RefrigeListPK 추가

--- backend/src/main/java/still88/backend/entity/RefrigeListPK.java
+++ backend/src/main/java/still88/backend/entity/RefrigeListPK.java
@@ -1,2 +1,19 @@
-package still88.backend.entity;public class RefrigeListPK {
+package still88.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class RefrigeListPK implements Serializable {
+    
+    private int refrigeId;
+
+    private int userId;
 }
