d7acc6e5d00c62ee430c65dbc0b7eae62c8e30cc
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-04-16 15:39:22 +0900

    feat: LikePK 설정

--- backend/src/main/java/still88/backend/entity/LikePK.java
+++ backend/src/main/java/still88/backend/entity/LikePK.java
@@ -1,6 +1,7 @@
 package still88.backend.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class LikePK implements Serializable {
 
     private int userId;
