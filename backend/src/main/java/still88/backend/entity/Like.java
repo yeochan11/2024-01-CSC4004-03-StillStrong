a5025e84367dab684fca46e4c76048204c82f866
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-04-16 15:30:07 +0900

    feat: Like Entity 추가

--- backend/src/main/java/still88/backend/entity/Like.java
+++ backend/src/main/java/still88/backend/entity/Like.java
@@ -1,4 +1,24 @@
 package still88.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@IdClass(LikePK.class)
+@Table(name="like")
+@Entity
 public class Like {
+    @Id
+    @Column(name="userId", nullable = false)
+    private int userId;
+
+    @Id
+    @Column(name="recipeId", nullable = false)
+    private int recipeId;
 }
