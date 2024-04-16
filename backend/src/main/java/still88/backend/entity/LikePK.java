1b11847db883877f6707d69d0d7c03a9c108e1c9
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-04-16 15:30:24 +0900

    feat: LikePK 추가

--- backend/src/main/java/still88/backend/entity/LikePK.java
+++ backend/src/main/java/still88/backend/entity/LikePK.java
@@ -1,2 +1,17 @@
-package still88.backend.entity;public class LikePK {
+package still88.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class LikePK implements Serializable {
+
+    private int userId;
+
+    private int recipeId;
 }
