e9dece3b3c321f8b86e4e8e86c988252d8218d37
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-04-16 15:29:00 +0900

    feat: IdPassword Entity 추가

--- backend/src/main/java/still88/backend/entity/IdPassword.java
+++ backend/src/main/java/still88/backend/entity/IdPassword.java
@@ -1,4 +1,29 @@
 package still88.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.lang.annotation.Target;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="idPassword")
+@Entity
 public class IdPassword {
+
+    @Id
+    @OneToOne
+    @JoinColumn(name="userId", nullable = false)
+    private User user;
+
+    @Column(name="id", nullable = false)
+    private String id;
+
+    @Column(name="password", nullable = false)
+    private String password;
 }
