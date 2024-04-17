43d86c7e736a55681fc3d0501e22e01506b5571c
mkchoii <kkungrr@gmail.com>
2024-04-17 16:43:07 +0900

    feat: appBarTheme 추가

--- fe_flutter/lib/main.dart
+++ fe_flutter/lib/main.dart
@@ -10,6 +10,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: WelcomePage(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          toolbarHeight: 58.0,
+          backgroundColor: const Color(0xffFFC94A),
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontFamily: 'Pretendard',
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          )
+        ),
+      ),
       routes: {
         '/signup': (context) => SignupPage(),
       },
@@ -20,7 +33,7 @@ class MyApp extends StatelessWidget {
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 10), () {
+    Future.delayed(Duration(seconds: 2), () {
       Navigator.pushReplacementNamed(context,'/signup');
     });
     return Scaffold(
@@ -34,7 +47,7 @@ class WelcomePage extends StatelessWidget {
                 children: <Widget>[
                   Image.asset('assets/images/welcomelogo.png'),
                   SizedBox(height: 20),
-                  Text("냉장고를 털고 싶을 땐?", style: TextStyle(fontFamily: 'Inter', fontSize: 16, fontWeight: FontWeight.w900, color: Colors.white)),
+                  Text("냉장고를 털고 싶을 땐?", style: TextStyle(fontFamily: 'Pretendard', fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)),
                   SizedBox(height: 3),
                   Image.asset('assets/images/yorijori.png'),
                 ],
