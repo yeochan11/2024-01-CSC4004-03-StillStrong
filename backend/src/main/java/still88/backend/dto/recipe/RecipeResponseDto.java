798b101992f5445aa82cb12d36029813a0d00c43
haeun1107 <haeun9394@gmail.com>
2024-04-19 21:50:08 +0900

    feat: Recipe responseDto 작성

--- backend/src/main/java/still88/backend/dto/recipe/RecipeResponseDto.java
+++ backend/src/main/java/still88/backend/dto/recipe/RecipeResponseDto.java
@@ -1,4 +1,28 @@
 package still88.backend.dto.recipe;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class RecipeResponseDto {
+    private int recipeId;
+    private String recipeName;
+    private String recipeCategory;
+    private String recipeDescription;
+    private String recipeImage;
+    private String recipeIngredient;
+    private int likeNum;
+
+    @Builder
+    public RecipeResponseDto(int recipeId, String recipeName, String recipeCategory, String recipeDescription, String recipeImage, String recipeIngredient, int likeNum) {
+        this.recipeId = recipeId;
+        this.recipeName = recipeName;
+        this.recipeCategory = recipeCategory;
+        this.recipeDescription = recipeDescription;
+        this.recipeImage = recipeImage;
+        this.recipeIngredient = recipeIngredient;
+        this.likeNum = likeNum;
+    }
 }
