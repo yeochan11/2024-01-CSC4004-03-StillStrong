a5d50dd09d3897855102fe4a8b52176c694cea28
haeun1107 <haeun9394@gmail.com>
2024-04-22 01:20:19 +0900

    feat: 재료 등록 request DTO 작성

---
+++ backend/src/main/java/still88/backend/dto/Ingredient/AddIngredientRequestDto.java
@@ -0,0 +1,19 @@
+package still88.backend.dto.Ingredient;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class AddIngredientRequestDto {
+    private int refrigeId;
+    private int userId;
+    private String ingredientName;
+    private String ingredientCategory;
+    private LocalDate createdDate;
+    private int ingredientNum;
+    private String ingredientPlace;
+    private LocalDate ingredientDeadline;
+    private String ingredientMemo;
+}
