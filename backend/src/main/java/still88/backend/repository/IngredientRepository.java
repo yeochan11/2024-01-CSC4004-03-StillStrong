b6ef4e45bc20bd5b43787cf8fa811e0610faef2c
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-04-30 23:54:29 +0900

    feat: 레포지토리 생성

---
+++ backend/src/main/java/still88/backend/repository/IngredientRepository.java
@@ -0,0 +1,7 @@
+package still88.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import still88.backend.entity.Ingredient;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+}
