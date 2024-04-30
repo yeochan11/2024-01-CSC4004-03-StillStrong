1a2d49e3fad3d7362caf04236c5a87a12f468b1f
haeun1107 <haeun9394@gmail.com>
2024-04-30 16:52:24 +0900

    refactor: 폴더 리펙토링

---
+++ backend/src/main/java/still88/backend/domain/refrige/RefrigeController.java
@@ -0,0 +1,16 @@
+package still88.backend.domain.refrige;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/refrige/create")
+public class RefrigeController {
+
+
+}
---
+++ backend/src/main/java/still88/backend/domain/user/UserController.java
@@ -0,0 +1,4 @@
+package still88.backend.domain.user;
+
+public class UserController {
+}
