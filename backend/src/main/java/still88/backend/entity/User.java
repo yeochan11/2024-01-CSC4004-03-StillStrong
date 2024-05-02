36a2c30ca33775a805bc7de9a9171590485b9da5
haeun1107 <haeun9394@gmail.com>
2024-05-02 15:43:32 +0900

    feat: User entity alarm 추가

--- backend/src/main/java/still88/backend/entity/User.java
+++ backend/src/main/java/still88/backend/entity/User.java
@@ -46,14 +46,18 @@ public class User{
     @Column
     private String userFavorite;
 
+    @Column
+    private Boolean alarm;
+
     // 생성자 + Builder로 일관성 유지
     @Builder
-    public User(String userNickname, int userAge, Boolean userGender, String userImage, String userAllergy, String userFavorite) {
+    public User(String userNickname, int userAge, Boolean userGender, String userImage, String userAllergy, String userFavorite, Boolean alarm) {
         this.userNickname = userNickname;
         this.userAge = userAge;
         this.userGender = userGender;
         this.userImage = userImage;
         this.userAllergy = userAllergy;
         this.userFavorite = userFavorite;
+        this.alarm = alarm;
     }
 }
