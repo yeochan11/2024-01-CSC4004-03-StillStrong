63816272249adeb8d653de95c6575ba4b11a409c
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-05-02 16:14:10 +0900

    feat: id pw 레포지토리 생성

---
+++ backend/src/main/java/still88/backend/repository/IdPasswordRepository.java
@@ -0,0 +1,9 @@
+package still88.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import still88.backend.entity.IdPassword;
+import still88.backend.entity.User;
+
+public interface IdPasswordRepository extends JpaRepository<IdPassword, User> {
+
+}
