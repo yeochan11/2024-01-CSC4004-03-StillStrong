5d5f41ccc3e3741ffc1661b6cd3966df43c9334c
mkchoii <kkungrr@gmail.com>
2024-05-13 10:47:11 +0900

    feat: Navigator 변경

--- fe_flutter/lib/main.dart
+++ fe_flutter/lib/main.dart
@@ -40,7 +40,7 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future.delayed(Duration(seconds: 2), () {
-      Navigator.pushReplacementNamed(context, '/register/category');
+      Navigator.pushReplacementNamed(context, '/login');
     });
     return Scaffold(
       backgroundColor: const Color(0xffFFC94A),
