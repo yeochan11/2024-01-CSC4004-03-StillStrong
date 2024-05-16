2a754d743b8924ddb5ec2a957660ade4ca57841e
ohgwanghoon <gwanghun3634@naver.com>
2024-05-16 15:41:15 +0900

    rename: 파일명 수정

---
+++ fe_flutter/lib/ingredientMoreInfo/deleteIngredient.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void deleteIngredient(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: true,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        actionsPadding: EdgeInsets.all(5.0),
+        elevation: 10.0,
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+        content: const Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text( //텍스트 위치 조정
+              "정말 삭제하시겠습니까?",
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(onPressed: () { // 재료 삭제 메소드 추가해야 함
+            Navigator.popUntil(context , ModalRoute.withName('/BottomMenu'));
+          },
+              child: const Text("확인", style: TextStyle(color: Colors.amber, fontWeight: FontWeight.bold))),
+          
+          TextButton(onPressed: () {
+            Navigator.of(context).pop();
+          },
+              child: const Text("취소", style: TextStyle(color: Colors.amber, fontWeight: FontWeight.bold)))
+        ],
+      );
+    }
+  );
+}
