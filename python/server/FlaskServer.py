d2070b4cd0149824a386e3986b88861f0b3af2a1
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-05-21 10:39:26 +0900

    feat: flask DB 연동

--- python/server/FlaskServer.py
+++ python/server/FlaskServer.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pickle
+import pymysql
 
 pickle_path = ""
 with open(pickle_path, 'rb') as file:
@@ -9,7 +10,25 @@ app = Flask(__name__)
 
 @app.route("/request-recommend", methods=['POST'])
 def recipe_recommend():
-    result = model.score()
+    conn = pymysql.connect(host='localhost', user='root', password='zpalq,123098!@#', db='still88', charset='utf8')
+    cursor = conn.cursor()
+
+    json_data = request.get_json()
+
+    user_id = json_data.get('userId')
+    ingredientList = json_data.get('ingredientList')
+
+    cursor.execute(f"SELECT userAge, userGender, userFavorite from User where userId = {user_id};")
+
+    result = cursor.fetchall()
+
+    user_info = {
+        "userAge": result[0],
+        "userGender": result[1],
+        "userFavorite": result[2]
+    }
+
+    result = model.score(ingredientList, user_info)
     return result
 
 if __name__ == '__main__':
