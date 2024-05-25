a9e11ea14e23be629ed088ce23afa3134b4633b3
ohgwanghoon <gwanghun3634@naver.com>
2024-05-25 19:55:56 +0900

    feat: 재료 상세 정보 API 함수 추가

--- fe_flutter/lib/service/db_server.dart
+++ fe_flutter/lib/service/db_server.dart
@@ -1,3 +1,4 @@
+import 'package:fe_flutter/model/ingredientMoreInfoModel.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:fe_flutter/model/user_model.dart';
@@ -19,5 +20,49 @@ Future<void> login(User user) async {
     }
   } catch(e) {
     print("Failed to send user data: &{e}");
+  }
+}
+
+Future<Map<String,dynamic>> fetchIngredientsInfo() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1')); //테스트 주소
+
+  if (response.statusCode == 200) {
+    final Map<String,dynamic> data = json.decode(response.body);
+    return data;
+  } else {
+    throw Exception('Failed to load data');
+  }
+}
+
+Future<void> deleteIngredientInfo() async {
+  final response = await http.delete(Uri.parse('https://jsonplaceholder.typicode.com/posts/1')); // 테스트 주소
+  if (response.statusCode == 200) {
+    print('Delect Sucessful');
+  } else {
+    throw Exception('Failed to delete data');
+  }
+}
+
+Future<void> patchIngredient(IngredientMoreInfoModel info) async {
+  final response = await http.patch(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'), //테스트 주소
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      }, // TODO: API 주소 연결했으면 밑에 주석 해제,
+      // body: jsonEncode(<String, dynamic>{
+      //   // 'ingredientPlace' : ingredientPlace,
+      //   // 'ingredientName' : ingredientName,
+      //   // 'createDate' : createdDate,
+      //   // 'ingredientDeadline' : ingredientDeadLine,
+      //   // 'ingredientNum' : ingredientNum,
+      //   'userId' : 5
+      // })
+      // TODO: 이건 주석 처리해주세요.
+    body: jsonEncode(info.toJson())
+  );
+  print('statusCode : ${response.statusCode}');
+  if (response.statusCode == 200) {
+    print('Patch successful');
+  } else {
+    throw Exception('Failed to patch data');
   }
 }
