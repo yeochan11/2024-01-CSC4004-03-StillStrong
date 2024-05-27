be4e63a9922f56ab9f670499859afa6f4eb53320
ohgwanghoon <gwanghun3634@naver.com>
2024-05-27 22:41:52 +0900

    feat: 메인페이지 구현 중

--- fe_flutter/lib/screens/mainPage.dart
+++ fe_flutter/lib/screens/mainPage.dart
@@ -1,12 +1,17 @@
+import 'package:fe_flutter/service/mainPageServer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fe_flutter/provider/userProvider.dart';
 
-//임시 페이지입니다.
+import '../model/mainPageModel.dart';
+
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user; // 유저 정보 불러오기
+    String searchRecipe = '';
+
+    //API로 값을 가져왔다고 가정
 
     return Scaffold(
       appBar: AppBar(
@@ -14,14 +19,70 @@ class MainPage extends StatelessWidget {
         title: Image.asset('assets/images/yorijori.png', scale: 1.5),
       ),
       body: Center(
-          child: user != null ? // 유저값이 null이 아닐 경우 내용 표시
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('메인 페이지\nEmail : ${user.secretEmail}\nPw : ${user.secretPassword}'), // 유저 정보 출력 (확인용)
-              ],
-            )
-              : null,
+        child: FutureBuilder(
+          future: fetchMainPageData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator(),);
+            } else {
+              mainPageModel info = mainPageModel(snapshot.data);
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding( //레시피 검색 위젯
+                    padding: EdgeInsets.all(5.0),
+                    child: SearchBar(
+                      trailing: [
+                        IconButton(
+                          onPressed: () {
+                            //TODO: 페이지 이동 추가
+                            print('TEST');},
+                          icon: const Icon(Icons.search),)
+                      ],
+                      shape: MaterialStateProperty.all(ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                      backgroundColor: const MaterialStatePropertyAll(Colors.white),
+                      side: MaterialStateProperty.all(
+                          const BorderSide(color: Color(0xffc7deff), width: 1.5)
+                      ),
+                      hintText: '레시피 검색',
+                      constraints: const BoxConstraints(maxHeight: 50, maxWidth: 330),
+                      elevation: const MaterialStatePropertyAll(0),
+                      onSubmitted: (value) {
+                        //TODO: 페이지 이동 추가
+                        print(value);
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  SizedBox(
+                    width: 400,
+                    height: 300,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Image.network(
+                        /*info.MainRecipeImage*/'https://recipe1.ezmember.co.kr/cache/recipe/2022/09/30/8e7eb8e3019532a8dc6d39a9a325aad41.jpg',
+                          fit: BoxFit.cover
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  
+                ],
+              );
+            }
+          },
+        ),
+          // child: user != null ? // 유저값이 null이 아닐 경우 내용 표시
+          //   Column(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: <Widget>[
+          //       Text('메인 페이지\nEmail : ${user.secretEmail}\nPw : ${user.secretPassword}'), // 유저 정보 출력 (확인용)
+          //     ],
+          //   )
+          //     : null,
           ),
       );
   }
--- fe_flutter/lib/service/ingredInfoServer.dart
+++ fe_flutter/lib/service/ingredInfoServer.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 
 Future<Map<String,dynamic>> fetchIngredientsInfo(int refrigeId, String ingredientName) async {
   String uri = 'https://jsonplaceholder.typicode.com/posts/1'; // 테스트 주소
-  //String uri = 'https://~~/refrige/ingredient/{refrigeId}?ingredientName={ingredientName}' //TODO: API 테스트시 이 주소를 이용해주세요.
+  //String uri = 'http//~~/refrige/ingredient/{refrigeId}?ingredientName={ingredientName}' //TODO: API 테스트시 이 주소를 이용해주세요.
   final response = await http.get(Uri.parse(uri));
   if (response.statusCode == 200) {
     final Map<String,dynamic> data = json.decode(response.body);
@@ -16,7 +16,7 @@ Future<Map<String,dynamic>> fetchIngredientsInfo(int refrigeId, String ingredien
 
 Future<void> deleteIngredientInfo(int refrigeId, int ingredientId) async {
   String uri = 'https://jsonplaceholder.typicode.com/posts/1'; // 테스트 주소
-  //String uri = 'https://~~/refrige/ingredient/delete/{refrigeId}/{ingredientId}' //TODO: API 테스트시 이 주소를 이용해주세요.
+  //String uri = 'http//~~/refrige/ingredient/delete/{refrigeId}/{ingredientId}' //TODO: API 테스트시 이 주소를 이용해주세요.
   final response = await http.delete(Uri.parse(uri));
   if (response.statusCode == 200) {
     print('Delect Sucessful');
@@ -28,7 +28,7 @@ Future<void> deleteIngredientInfo(int refrigeId, int ingredientId) async {
 Future<void> patchIngredient(IngredientMoreInfoModel info, int refrigeId) async {
   String uri = 'https://jsonplaceholder.typicode.com/posts/1'; // 테스트 주소
   print('${info.ingredientId}        ${refrigeId}');
-  //String uri = 'https://~~/refrige/ingredient/{info.refrigeId}/{info.ingredientId}/edit'; //TODO: API 테스트시 이 주소를 이용해주세요.
+  //String uri = 'http//~~/refrige/ingredient/{info.refrigeId}/{info.ingredientId}/edit'; //TODO: API 테스트시 이 주소를 이용해주세요.
   final response = await http.patch(Uri.parse(uri),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
