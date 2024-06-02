891beecc03d55d358dd27863728b7acfdfa457fc
gnvvoo <t412@naver.com>
2024-06-02 16:09:17 +0900

    design: MyRefrig 디자인 수정

--- fe_flutter/lib/screens/MyRefrigerator/empty.dart
+++ fe_flutter/lib/screens/MyRefrigerator/empty.dart
@@ -1,5 +1,4 @@
 import 'package:fe_flutter/screens/ingredientRegister/ingredientRegister.dart';
-import 'package:fe_flutter/service/refrigeServer.dart';
 import 'package:flutter/material.dart';
 import '../ingredientMoreInfo/ingredientMoreInfo.dart';
 import 'myRefrigeratorDropdown.dart';
@@ -11,23 +10,48 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class MyRefrigPage extends StatefulWidget {
   @override
-  MyRefrigPageState createState() => MyRefrigPageState();
+  _MyRefrigPageState createState() => _MyRefrigPageState();
 }
 
-class MyRefrigPageState extends State<MyRefrigPage> {
-  List<Map<String, dynamic>> refrigeList = [];
-  int currentRefrigeId = 1;
-  List<String> newItems = ["기본 냉장고"]; // 기본값으로 초기화합니다.
+class _MyRefrigPageState extends State<MyRefrigPage> {
 
+  List<Map<String, dynamic>> refrigeList = [];
+//TODO: 현재 선택 중인 냉장고 인덱스, 냉장고 드롭다운이랑 연결해서 값을 받을 수 있도록 수정 부탁드립니다.
+  int currentRefrigeId = 1;
   @override
   void initState() {
     super.initState();
+    fetchRefrigeList();
   }
+
+  Future<void> fetchRefrigeList() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    int? userId = pref.getInt("userId");
+    final url = 'http://localhost:8080/refrige/get/refrigeWithIngredients?userId=$userId';
+    try {
+      final response = await http.get(Uri.parse(url), headers: {"Accept": "application/json; charset=utf-8"});
+      if (response.statusCode == 200) {
+        final responseBody = utf8.decode(response.bodyBytes);
+        final data = json.decode(responseBody);
+        setState(() {
+          refrigeList = List<Map<String, dynamic>>.from(data['refrigeList'].map((item) => Map<String, dynamic>.from(item)));
+          currentRefrigeId = data['currentRefrigeId'];
+        });
+        print(refrigeList);
+        print(currentRefrigeId);
+      } else {
+        print('Failed to load refrige list');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    List<dynamic> ingredients = refrigeList.map<String>((refrige) => refrige['ingredientNames'] as String).toList();
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -42,12 +66,17 @@ class MyRefrigPageState extends State<MyRefrigPage> {
           centerTitle: true,
           backgroundColor: Color(0Xffffc94a),
         ),
-        body: Container(
+        body: Padding(
+          padding: EdgeInsets.all(23),
           child: Column(
             children: [
+              SizedBox(height: 32),
               DropdownRefrige(),
+              SizedBox(height: 20),
               IngredientSearch(),
+              SizedBox(height: 39),
               IngredientSelect(),
+              SizedBox(height: 28),
               Container(
                 alignment: Alignment.center,
                 width: 340,
@@ -57,16 +86,16 @@ class MyRefrigPageState extends State<MyRefrigPage> {
                       children:
                         ingredients.map((ingredient){
                           return IngredIconButton(
-                              buttonText: ingredient['ingredientNames'],
-                              expDate: 16,
-                              icon: Image.asset('assets/images/ingredient.png'),
-                              onPressed: (isIngredientSelect, isPressed, buttonText) {
-                                if (!isIngredientSelect) {
-                                  showInfo(currentRefrigeId, buttonText);
-                                }
-                              },
-                            );
-                          }).toList(),
+                            buttonText: ingredient['ingredientNames'],
+                            expDate: 16,
+                            icon: Image.asset('assets/images/ingredient.png'),
+                            onPressed: (isIngredientSelect, isPressed, buttonText) {
+                              if (!isIngredientSelect) {
+                                showInfo(currentRefrigeId, buttonText);
+                            }
+                          },
+                        );
+                      }).toList(),
                     )
                   ],
                 ),
@@ -74,6 +103,12 @@ class MyRefrigPageState extends State<MyRefrigPage> {
             ],
           ),
         ),
+        /* floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showPopup(context);
+          },
+          child: Icon(Icons.add),
+        ), */
         floatingActionButton: Builder(
           builder: (context) => FloatingActionButton(
             onPressed: () async {
@@ -159,6 +194,12 @@ class MyRefrigPageState extends State<MyRefrigPage> {
                   case 1:
                     break;
                   case 2:
+                    break;
+                  case 3:
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => IngredRegPage()),
+                    );
                     break;
                 }
               }
