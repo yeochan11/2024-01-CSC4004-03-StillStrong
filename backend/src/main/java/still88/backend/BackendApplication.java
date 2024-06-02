f2785015e0861656465f8b2cdb10a3308b640bbc
Johyeonwoooooooo <hyunwoo02031@naver.com>
2024-06-02 16:10:53 +0900

    feat: 충돌 제거

--- backend/src/main/java/still88/backend/BackendApplication.java
+++ backend/src/main/java/still88/backend/BackendApplication.java
@@ -3,11 +3,6 @@ package still88.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackendApplication {
