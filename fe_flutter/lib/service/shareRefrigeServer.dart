e7c34939c2f7db0cb5742bbab6d950e21bd43794
mkchoii <kkungrr@gmail.com>
2024-06-04 23:18:13 +0900

    feat: shearchedUserModel 추가

--- fe_flutter/lib/service/shareRefrigeServer.dart
+++ fe_flutter/lib/service/shareRefrigeServer.dart
@@ -1,36 +1,27 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fe_flutter/model/searchedUserModel.dart';
 
 // 사용자 검색
-Future<Map<String, dynamic>> searchUser(String searchName) async {
-  Map<String, dynamic> requestBody = {
-    "serachName": searchName,
+Future<SearchedUser> searchUser(String searchName) async {
+  Map<String, String> requestBody = {
+    "searchName": searchName,
   };
   try {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    int? userId = pref.getInt("userId");
     final response = await http.post(
-      Uri.parse('http://localhost:8080/share/invite/search'),
+      Uri.parse('http://localhost:8080/share/invite/search?userId=${userId}'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=utf-8',
       },
       body: jsonEncode(requestBody),
     );
     if (response.statusCode == 200) {
-      Map<String, dynamic> responseData = jsonDecode(response.body);
-      // 받은 데이터 저장
-      String searchUserImage = responseData['searchUserImage'];
-      List<String> refrigeNames = responseData['refrigeNames'];
-      List<int> refrigeIds = responseData['refrigeIds'];
-
-      print('searchUserImage : ${searchUserImage}');
-      print('refrigeNames : ${refrigeNames}');
-      print('refrigeIds : ${refrigeIds}');
-
-      return {
-        'searchUserImage': searchUserImage,
-        'refrigeNames': refrigeNames,
-        'refrigeIds': refrigeIds,
-      };
-
+      final decodeData = utf8.decode(response.bodyBytes);
+      Map<String, dynamic> data = jsonDecode(decodeData);
+      return SearchedUser.fromJson(data);
     } else {
       throw Exception('Failed to load data');
     }
